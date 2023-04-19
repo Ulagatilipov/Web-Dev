d68eac1ef8aeb085c80ebd00de737ddeff75500e
Ula <98708473+Ulagatilipov@users.noreply.github.com>
2023-04-19 16:06:28 +0600

    Add files via upload

---
+++ lab8/shop_back/api/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, default='')
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=251, default='')
+    price = models.FloatField(max_length=250, default='')
+    description = models.TextField()
+    count = models.IntegerField(default=0)
+    is_active = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active,
+            'category': self.category.name
+        }
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
